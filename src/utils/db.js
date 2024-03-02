b27a26970edfd18168a1a9f9a95c5e61f8c2f1ef
Omar Crespo <crespomerchano@gmail.com>
2024-03-02 16:19:54 -0500

    adding logs to lambda

--- src/utils/db.js
+++ src/utils/db.js
@@ -5,12 +5,16 @@ mongoose.Promise = global.Promise;
 let isConnected;
 
 module.exports = connectToDatabase = () => {
+  console.log('isConnected:', isConnected);
+
   if (isConnected) {
+    console.log('=> using existing database connection');
     return Promise.resolve();
   }
 
   return mongoose.connect(process.env.MONGO_URL)
     .then(db => {
+      console.log('DB connected');
       isConnected = db.connections[0].readyState;
     });
 };
